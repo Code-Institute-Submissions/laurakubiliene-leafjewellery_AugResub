636050a1ed2eba93ee9613e1b9e407c883a0a4ec
Laura Kubiliene <lauroslapai1@gmail.com>
2021-06-11 16:37:44 +0000

    fixed the template issues

--- leafjewellery/settings.py
+++ leafjewellery/settings.py
@@ -61,11 +61,10 @@ ROOT_URLCONF = 'leafjewellery.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
-        'APP_DIRS': [
+        'DIRS': [
             os.path.join(BASE_DIR, 'templates'),
-            os.path.join(BASE_DIR, 'templates', 'allauth'),
-        ],
+            os.path.join(BASE_DIR, 'templates', 'allauth')],
+        'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
                 'django.template.context_processors.debug',
@@ -150,6 +149,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
