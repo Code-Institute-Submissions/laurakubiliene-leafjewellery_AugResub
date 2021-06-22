5db73b71cc592be9d4ff44693c85ce2a3704b2c6
Laura Kubiliene <lauroslapai1@gmail.com>
2021-06-22 19:56:24 +0000

    comments app

---
+++ comments/views.py
@@ -0,0 +1,4 @@
+from django.shortcuts import render, redirect, reverse, get_object_or_404, HttpResponse
+
+# Create your views here.
+
--- leafjewellery/settings.py
+++ leafjewellery/settings.py
@@ -47,7 +47,7 @@ INSTALLED_APPS = [
     'products',
     'bag',
     'checkout',
-    'create',
+    'comments',
     # Other
     'crispy_forms',
 
