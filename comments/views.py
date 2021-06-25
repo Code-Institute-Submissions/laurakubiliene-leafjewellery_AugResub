618bcdf6fbd124356ad5d9f85d354b01e1e2eaf7
Laura Kubiliene <lauroslapai1@gmail.com>
2021-06-25 14:13:14 +0000

    changed comments form and solved the fixed position on header

--- comments/views.py
+++ comments/views.py
@@ -1,4 +1,31 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib import messages
+
+from .forms import CommentForm
+
+
+def comments(request):
+    if request.method == 'POST':
+        form = CommentForm(request.POST, request.FILES)
+        if form.is_valid():
+            product = form.save()
+            messages.success(request, 'Successfully added comment!')
+            return redirect(reverse('comments'))
+        else:
+            messages.error(request, 'Failed to add comment. Please ensure the form is valid.')
+    else:
+        form = CommentForm()
+
+    template = 'comments/comments.html'
+    context = {
+        'form': form,
+    }
+
+    return render(request, template, context)
+
+
+
+
 
 
 def comments(request):
