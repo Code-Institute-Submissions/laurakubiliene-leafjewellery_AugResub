f55b00a4cb6633a71c863903181211c284626a78
Laura Kubiliene <lauroslapai1@gmail.com>
2021-06-28 21:01:27 +0000

    secret key changes

--- leafjewellery/settings.py
+++ leafjewellery/settings.py
@@ -23,10 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-p7lt-jfg)m=yxzg94f$5vci+!&2%69%3sabslhxe))21$q=+q='
+SECRET_KEY = SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['leaf-jewellery.herokuapp.com', 'localhost']
 
