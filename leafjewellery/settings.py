a746e3d1454b6b1371d3ed879357442389732d87
Laura Kubiliene <lauroslapai1@gmail.com>
2021-08-24 14:07:27 +0000

    changed email password

--- leafjewellery/settings.py
+++ leafjewellery/settings.py
@@ -216,7 +216,7 @@ STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
 
 if 'DEVELOPMENT' in os.environ:
     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-    DEFAULT_FROM_EMAIL = 'leafjewellery@example.com'
+    DEFAULT_FROM_EMAIL = 'lauroslapai1@gmail.com'
 else:
     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
     EMAIL_USE_TLS = True
